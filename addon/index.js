9bf5577f4aeeb4330a14d7bbd48eb25030ec539a
Tobias Bieniek <tobias@bieniek.cloud>
2022-11-02 15:21:57 +0100

    Add `method` validity assertions

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,25 @@
+import { assert } from '@ember/debug';
+
+const VALID_METHODS = [
+  'GET',
+  'HEAD',
+  'POST',
+  'PUT',
+  'DELETE',
+  'OPTIONS',
+  'PATCH',
+];
+
 export async function apiAction(record, { method, path, data }) {
+  assert(`Missing \`method\` option`, method);
+  assert(
+    [
+      `Invalid \`method\` option: ${method}`,
+      `Valid options: ${VALID_METHODS.join(', ')}`,
+    ].join('\n'),
+    VALID_METHODS.includes(method)
+  );
+
   let modelClass = record.constructor;
   let modelName = modelClass.modelName;
   let adapter = record.store.adapterFor(modelName);
